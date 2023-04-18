583a5077d7f3c73b643c70d385e1efbefc8aa9ba
evgeny_sem163 <evg82tol@gmail.com>
2023-04-18 20:48:09 +0300

    All files

---
+++ src/main/java/sky/com/employeebook_2/Employeebook2Application.java
@@ -0,0 +1,13 @@
+package sky.com.employeebook_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Employeebook2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Employeebook2Application.class, args);
+	}
+
+}
---
+++ src/test/java/sky/com/employeebook_2/Employeebook2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.com.employeebook_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Employeebook2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
